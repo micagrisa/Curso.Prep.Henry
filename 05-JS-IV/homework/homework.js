c785505160e46674f40099c9b453e521e5d1a3fa
micaela grisapulli <micaelabelengrisapulli@gmail.com>
2022-05-11 16:24:42 -0300

    test 2

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -20,7 +20,9 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
-}
+  objeto [property] = "null"
+  return objeto
+};
 
 function invocarMetodo(objeto, metodo) {
   // "metodo" es una cadena que contiene el nombre de un método (funcion) en el objeto
