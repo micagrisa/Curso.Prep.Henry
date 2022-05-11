3fd772e7749ee9b5720c44d789c473eef4ff535b
micaela grisapulli <micaelabelengrisapulli@gmail.com>
2022-05-11 16:45:17 -0300

    test 8

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -36,7 +36,8 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
- return (objetoMisterioso[numeroMisterioso]* 5)
+ var resultado = objetoMisterioso.numeroMisterioso * 5;
+ return resultado
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
