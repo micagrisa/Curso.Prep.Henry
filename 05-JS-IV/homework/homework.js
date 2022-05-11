01e625cad9e6c8461d501ec2cc1be78b6e34b0af
micaela grisapulli <micaelabelengrisapulli@gmail.com>
2022-05-11 17:15:09 -0300

    test 9

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -45,8 +45,8 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
-  delete objeto[unaPropiedad];
-  return objeto[unaPropiedad]
+  delete objeto[unaPropiedad],
+  return objeto
 }
 
 function nuevoUsuario(nombre, email, password) {
